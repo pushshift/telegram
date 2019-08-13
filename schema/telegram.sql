50219d918a37a9d9fa312601614c529506e2a9e8
Jason Baumgartner <jason@pushshift.io>
2019-08-13 21:19:17 +0000

    Postgres DB sql schema

---
+++ schema/telegram.sql
@@ -0,0 +1,71 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.5 (Ubuntu 11.5-0ubuntu0.19.04.1)
+-- Dumped by pg_dump version 11.5 (Ubuntu 11.5-0ubuntu0.19.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: channel; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.channel (
+    id bigint NOT NULL,
+    retrieved_utc integer,
+    is_active boolean,
+    data jsonb
+);
+
+
+ALTER TABLE public.channel OWNER TO postgres;
+
+--
+-- Name: message; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.message (
+    id bigint NOT NULL,
+    message_id integer,
+    channel_id bigint,
+    retrieved_utc integer,
+    updated_utc integer
+);
+
+
+ALTER TABLE public.message OWNER TO postgres;
+
+--
+-- Name: channel channel_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.channel
+    ADD CONSTRAINT channel_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: message message_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.message
+    ADD CONSTRAINT message_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
