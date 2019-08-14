17cf6c2c2e11d545894f99e3e527a5f1f1c79237
Savvas Zannettou <zsavvas90@gmail.com>
2019-08-14 11:53:36 +0300

    added support to search channels using Lyzem search engine

--- channels.py
+++ channels.py
@@ -11,6 +11,8 @@ from urlparse import urlparse
 
 
 TELEGAGO_BASE_URL = 'https://cse.google.com/cse?q=+&cx=006368593537057042503:efxu7xprihg#gsc.tab=0&gsc.ref=more%3Apublic&gsc.q='
+LYZEM_BASE_URL = 'https://lyzem.com/search?f=channels&l=%3Aen&per-page=100&q='
+
 
 # extracts the html from a URL using the requests_html library (supports JS)
 def extract_html(url, javascript_enabled=False):
@@ -22,7 +24,68 @@ def extract_html(url, javascript_enabled=False):
         source_html = response.html.html
         return source_html
     else:
-        return response.html
+        return response.html.html
+
+
+# method to parse the HTML from the Lyzem page
+def parse_lyzem_page(html):
+    print(html)
+    soup = BeautifulSoup(html, "lxml")
+    links = soup.find_all('li', attrs={'class', 'result'})
+    channels = []
+    for link in links:
+        try:
+            print(link['data-url'])
+            print(type(link['data-url']))
+            element_classes = link['class']
+            # if they have this element this means the result is an advertisement
+            # we dont want these
+            if 'ann' in element_classes:
+                continue
+            path_url = link['data-url']
+            channel_name = path_url.split('?')[0].split('/')[-1]
+            if channel_name not in channels:
+                channels.append(channel_name)
+        except KeyError:
+            continue
+    return channels
+
+def search_channels_lyzem(query, limit=100):
+    all_channels = []
+    initial_request_url = LYZEM_BASE_URL + urllib.parse.quote(query)
+    logging.debug("Lyzem request url {}".format(initial_request_url))
+
+    # extract channels from initial page
+    source_html = extract_html(initial_request_url, javascript_enabled=False)
+    print(source_html)
+    page_channels = parse_lyzem_page(source_html)
+    all_channels = page_channels
+    
+    # if reached limit return the channels
+    if len(all_channels)>=limit:
+        return all_channels[:limit]
+
+    # otherwise we need to go to next pages
+    # find the number of pages from the html
+    soup = BeautifulSoup(source_html, "lxml")
+    cursor_div = soup.find_all('nav', {'class': 'pages'})
+    num_pages = len(cursor_div[0].find_all('li'))
+    
+    # then iterate over all pages to extract all channels
+    i=1
+    for i in range(num_pages):
+        request_url = initial_request_url + '&p=' + str(i+1)
+        logging.debug("Lyzem request url {}".format(request_url))
+        source_html = extract_html(request_url, javascript_enabled=False)
+        page_channels = parse_lyzem_page(source_html)
+        for channel in page_channels:
+            if channel not in all_channels:
+                all_channels.append(channel)
+        i+=1
+        if len(all_channels)>=limit:
+            return all_channels[:limit]
+    return all_channels
+
 
 # method to parse the HTML from the telegago page
 def parse_telegago_page(html):
